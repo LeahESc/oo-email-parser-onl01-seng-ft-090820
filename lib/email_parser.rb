60bf98b67361d256d94eec2b7cd1d26e8d93f28a
Leah Schlackman <leahemily1@gmail.com>
2020-09-18 17:00:04 +0000

    Automatically backed up by Learn

--- lib/email_parser.rb
+++ lib/email_parser.rb
@@ -7,10 +7,10 @@ require 'pry'
 class EmailAddressParser
   attr_accessor :email_addresses
   
-  # def initialize(email_addresses)
-  #   @email_addresses = email_addresses
-  # end
-  # binding.pry
+  def initialize(email_addresses)
+    @email_addresses = email_addresses
+  end
+  binding.pry
   
   def parse(email_addresses)
     email_array = email_addresses.split(/[\s,]/)
